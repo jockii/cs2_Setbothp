e0a98aa87d69b9b94a32c8bbcf649143faec24fe
jockii <konon3nko98@gmail.com>
2023-11-22 01:52:51 +0200

    fix command "setbothp"

--- Setbothp.cs
+++ Setbothp.cs
@@ -1,39 +1,30 @@
 ﻿using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
 using CounterStrikeSharp.API.Modules.Commands;
-
+using CounterStrikeSharp.API.Modules.Utils;
+using System.Drawing;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
 
 namespace Setbothp;
-public class Setbothp : BasePlugin, IPluginConfig<SetbothpConfig>
+
+[MinimumApiVersion(65)]
+public class Setbothp : BasePlugin
 {
     public override string ModuleName => "Setbothp";
 
     public override string ModuleVersion => "v1.1.0";
-    public override string ModuleAuthor => "\"jackson tougher\"";
-
-    public SetbothpConfig Config { get; set; } = new();
-
-    private int _SET_BOT_HP = 0;
-
+    public override string ModuleAuthor => "| jackson tougher |";
     public override void Load(bool hotReload)
     {
         Console.WriteLine($"Plugin [Setbothp] {ModuleVersion} by {ModuleAuthor} was loaded =D");
     }
-
-    public void OnConfigParsed(SetbothpConfig config)
+    public void LogToChatAll(string msg)
     {
-        this.Config = config;
-        _SET_BOT_HP = config.SET_BOT_HP;
-    }
-
-    public async void Pause()
-    {
-        await Task.Delay(3000);
-    }
-    public void Log(string msg) 
-    { 
-        Server.PrintToChatAll(msg); 
+        Server.PrintToChatAll(msg);
     }
     public void SendConsoleCommand(string command)
     {
@@ -43,37 +34,45 @@ public class Setbothp : BasePlugin, IPluginConfig<SetbothpConfig>
     public const int STANDART_BOT_HP = 100;
     public const int MIN_BOT_HP = 1;
     public const int MAX_BOT_HP = 9999999;
+    public int SET_BOT_HP = 0;
 
-    [ConsoleCommand("css_setbothp_reload")] 
+    [ConsoleCommand("css_setbothp")]
     public void OnCommandSetBotHp(CCSPlayerController? controller, CommandInfo command)
     {
         if (controller == null) return;
-        SendConsoleCommand("css_plugins stop Setbothp");
-        Pause();
-        SendConsoleCommand("css_plugins start Setbothp");
-        controller.PrintToChat("[Setbothp] config reload... OK!");
-        //command.ReplyToCommand("");
-
-        const string msg = "[Setbothp] configuration successfully rebooted!";
-        SendConsoleCommand(msg);
+        if (controller.SteamID == 1111111111111 || controller.SteamID == 00000000000000) 
+        {
+            if (Regex.IsMatch(command.GetArg(1), @"^\d+$"))
+            {
+                SET_BOT_HP = int.Parse(command.GetArg(1));
+                controller.PrintToChat($"{ChatColors.Red}[Setbothp]{ChatColors.Olive}config reload... {ChatColors.Green}OK!");
+                controller.PrintToChat($"New Bot HP: {ChatColors.Green}{SET_BOT_HP}");
+            }
+            else
+            {
+                SET_BOT_HP = STANDART_BOT_HP;
+                controller.PrintToChat($"{ChatColors.Red}Incorrect value! Please input correct number");
+            }
+        }
+        else
+            controller.PrintToChat($"{ChatColors.Red}You are not Admin!!!");
     }
-
-    public void SetBotHp(List<CCSPlayerController> playersList) 
+    public void SetBotHp(List<CCSPlayerController> playersList, int bot_hp)
     {
         playersList.ForEach(player =>
         {
             if (player.IsBot)
             {
-                //Log("--- find bot");
-                if (_SET_BOT_HP >= MIN_BOT_HP  || _SET_BOT_HP <= MAX_BOT_HP)
+                //LogToChatAll("--- find bot");
+                if (bot_hp >= MIN_BOT_HP && bot_hp <= MAX_BOT_HP)
                 {
-                    player.Pawn.Value.Health = _SET_BOT_HP;
-                    //Log($"--- set bot HP to {_INPUT_BOT_HP}HP <--");
+                    player.Pawn.Value.Health = bot_hp;
+                    //($"--- set bot HP to {bot_hp}HP <--");
                 }
-                else if (_SET_BOT_HP < MIN_BOT_HP || _SET_BOT_HP > MAX_BOT_HP)
+                else if (bot_hp < MIN_BOT_HP || bot_hp > MAX_BOT_HP)
                 {
                     player.Pawn.Value.Health = STANDART_BOT_HP;
-                    //Log($"else if === STANDART_BOT_HP");
+                    //LogToChatAll($"else if === STANDART_BOT_HP");
                     SendConsoleCommand($"[{ModuleName}] incorrect value. Bot health set to standart value: 100HP");
                 }
             }
@@ -82,10 +81,11 @@ public class Setbothp : BasePlugin, IPluginConfig<SetbothpConfig>
     [GameEventHandler]
     public HookResult OnRoundStart(EventRoundStart @event, GameEventInfo @info)
     {
-        //Log("--- Round Event  ---");
+        LogToChatAll(SET_BOT_HP.ToString());
+        //LogToChatAll("--- Round Event  ---");
         var players = Utilities.GetPlayers();
-        SetBotHp(players);
-        //Log("-- call func SetBotHp() on start round ");
+        SetBotHp(players, SET_BOT_HP);
+        //LogToChatAll("-- call func SetBotHp() on start round ");
         return HookResult.Continue;
     }
 }
